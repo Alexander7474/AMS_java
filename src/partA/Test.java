23d037fbeac5feacac308787e1307bc07c88d3be
cafidy <122469310+cafidy@users.noreply.github.com>
2024-11-30 11:15:51 +0100

    Add files via upload

--- src/partA/Test.java
+++ src/partA/Test.java
@@ -5,10 +5,21 @@ import partB.*;
 import partC.*;
 
 public class Test {
-	public static void main(String[] args) {
-		Corpus c = new Corpus("src/booksummaries_sample.txt", DataSets.BOOK);
-		TfIdf t = new TfIdf();
-		TfIdf t2 = t.processCorpus(c);
-		t2.processQuery("The story is presented by Le Fanu", 3);
-	}
+    public static void main(String[] args) {
+
+        Corpus c = new Corpus("src/booksummaries_sample.txt", DataSets.BOOK);
+
+        Bm25 bm25 = new Bm25();
+
+        bm25 = bm25.processCorpus(c);
+
+        String query = "The story is presented by Le Fanu";
+        int maxDocsToShow = 3;
+
+        System.out.println("Top " + maxDocsToShow + " Documents:");
+        bm25.processQuery(query, maxDocsToShow);
+    }
 }
+
+
+
