1d810bcd7c44a566151dc0a581445c44d8a6c1b6
cafidy <122469310+cafidy@users.noreply.github.com>
2024-12-02 13:15:09 +0100

    Add files via upload

---
+++ src/test/TestCorpus.java
@@ -0,0 +1,43 @@
+package test;
+
+import partA.*;
+import partB.*;
+import partC.*;
+import partE.CorpusExc;
+import partE.MoteurRechercheExc;
+
+/**
+ * @author Yassine EL MSEBLI
+ */
+
+/**
+ * @brief test l'exception CorpusExc verifiant l existence du ficher et qu il ne soit pas vide
+
+ */
+public class TestCorpus {
+	public static void Corpustest(String args) throws MoteurRechercheExc {
+		Corpus c = null ;
+		try {
+			c=new Corpus("src/empty.txt", DataSets.BOOK);
+		}catch(CorpusExc cd) {
+			throw new CorpusExc ("the file do not exist or is empty");
+		}
+				
+		TfIdf t = new TfIdf();
+		StopList stpL = new StopList("src/stopWords.txt");
+		
+		long start = System.nanoTime();
+		TfIdf tfStopList = t.processCorpus(c, stpL);
+		long end = System.nanoTime();
+		System.out.println("Temps de proces du corpus STOP LIST :" + ((end-start)*(Math.pow(10, -9))));
+		
+		start = System.nanoTime();
+		tfStopList.processQuery("covering the period from the Decree of Cyrus to the dedication of the Second Temple", 1);
+		tfStopList.processQuery("Beginning several months after the events in Blade Runner", 1);
+		tfStopList.processQuery("Wells grows up in an orphanage where he spends his childhood", 1);
+		end = System.nanoTime();
+		
+		System.out.println("Temps de proces query STOP LIST :" + ((end-start)*(Math.pow(10, -9))));
+		System.out.println(tfStopList.getVoc().getSize());
+	}
+}
